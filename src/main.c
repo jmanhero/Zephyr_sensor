7c4e19176a59236f01bd28e23bb1855a2078f7d4
Jason Lambert <jsonwlambert@gmail.com>
2021-10-26 14:56:07 -0700

    refactored to use 3 threads and blink 2 leds. a data stream is being sent out the uart as well.

--- src/main.c
+++ src/main.c
@@ -2,55 +2,122 @@
 #include <device.h>
 #include <devicetree.h>
 #include <drivers/gpio.h>
+#include <sys/printk.h>
+#include <sys/__assert.h>
+#include <string.h> 
 
-#define SLEEP_TIME_MS 500
+
+/* size of the stack for each thread */ 
+#define STACKSIZE 1024
+
+#define PRIORITY 7
 
 // The devicetree node identifier for the "led0" alias.
 #define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
 
-#if DT_NODE_HAS_STATUS(LED0_NODE, okay)
-#define LED0 	DT_GPIO_LABEL(LED0_NODE, gpios)
-#define PIN 	DT_GPIO_PIN(LED0_NODE,gpios)
-#define FLAGS	DT_GPIO_FLAGS(LED0_NODE,gpios)
-#else
 
-//any build error here mean something in the board isn't set up to blink an led
-#error "unsupported board: LED devicetree alias is no defined"
-#define LED0	""
-#define PIN	0
-#define FLAGS	0
+#if !DT_NODE_HAS_STATUS(LED0_NODE, okay)
+#error "the board doesn't have an allias that matches led0"
 #endif
 
+#if !DT_NODE_HAS_STATUS(LED1_NODE,okay)
+#error "the board doesn't have an allias that matches led0"
+#endif
+
+//define the data package to be inserted into the fifo by each thread every time an event happens 
+struct printk_data_t {
+	void *fifo_reserved; //the first word most be reserved in the fifo <-- test this later
+	uint32_t led;
+	uint32_t cnt; 
+};
+
+//initialize the FIFO
+K_FIFO_DEFINE(printk_fifo);
+
+//define a structure that represents the LEDs 
+struct led {
+	struct gpio_dt_spec spec;
+	const char *gpio_pin_name;
+};
+
+//initialize my leds structures
+static const struct led led0 = {
+	.spec = GPIO_DT_SPEC_GET_OR(LED0_NODE,gpios,{0}),
+	.gpio_pin_name = DT_PROP_OR(LED0_NODE,label,""),
+};
+
+static const struct led led1 = {
+	.spec = GPIO_DT_SPEC_GET_OR(LED1_NODE,gpios,{}),
+	.gpio_pin_name = DT_PROP_OR(LED1_NODE,label,""),
+};
 
 
-
-void main(void)
+void blink(const struct led * led, uint32_t sleep_time_ms, uint32_t id)
 {
+	const struct gpio_dt_spec *spec = &led->spec;
+	int cnt = 0;
+	int ret; 
 
-
-
-	const struct device *dev;
-	bool led_is_on = true; 
-	int ret;
-
-	dev = device_get_binding(LED0);
-	if( dev == NULL)
-	{
+	//check to see if the gpio peripherial has been turned on
+	if(!device_is_ready(spec->port)){
+		printk("Error: %s device is not ready", spec->port->name);
 		return;
 	}
 
-		ret = gpio_pin_configure(dev, PIN, GPIO_OUTPUT_ACTIVE | FLAGS);
-	if(ret < 0)
-	{
+	// check to see if the configuration of the port was successful 
+	ret = gpio_pin_configure_dt(spec,GPIO_OUTPUT);
+	if(ret != 0){
+		printk("Error %d: failed to configure pin %d (LED '%s')\n",
+					ret,spec->pin,led->gpio_pin_name);
 		return;
 	}
 
-	while(1)
-	{
-		gpio_pin_set(dev,PIN,(int)led_is_on);
-		led_is_on = !led_is_on;
-		k_msleep(SLEEP_TIME_MS);
+	while(1){
+
+		gpio_pin_set(spec->port,spec->pin,cnt % 2 );
+
+		struct printk_data_t tx_data = {.led = id, .cnt = cnt};
+
+		size_t size = sizeof(struct printk_data_t);
+
+		char *memptr = k_malloc(size);
+
+		__ASSERT_NO_MSG(memptr != 0 );
+
+		memcpy(memptr,&tx_data,size);
+
+		k_fifo_put(&printk_fifo,memptr);
+
+		k_msleep(sleep_time_ms);
+
+		cnt++;
+
 	}
 }
 
 
+void blink0(void){
+	blink(&led0,100,0);
+}
+
+void blink1(void){
+	blink(&led1,500,1);
+}
+
+void uart_out(void){
+	
+	while(1){
+		struct printk_data_t *rx_data = k_fifo_get(&printk_fifo,K_FOREVER); // pull the data out of the fifo
+
+		printk("Toggled led %d. Counter = %d\n",rx_data->led,rx_data->cnt);
+
+		k_free(rx_data);
+
+	}
+
+}
+
+K_THREAD_DEFINE(blink0_id,STACKSIZE,blink0,NULL,NULL,NULL,PRIORITY,0,0);
+K_THREAD_DEFINE(blink1_id,STACKSIZE,blink1,NULL,NULL,NULL,PRIORITY,0,0);
+K_THREAD_DEFINE(uart_out_id,STACKSIZE,uart_out,NULL,NULL,NULL,PRIORITY,0,0);
